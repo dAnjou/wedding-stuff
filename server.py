c358ec7108c721e18d8a5667e793de4fadc3d1a3
Max Ludwig <maxe.ludwig@googlemail.com>
2012-08-08 00:53:02 +0200

    replaced flask-uploads with hand-written code

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, jsonify, redirect, request, Markup, escape, Response
+from flask import Flask, render_template, jsonify, redirect, request, Markup, escape, Response, send_from_directory
+from werkzeug import secure_filename
 
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.uploads import UploadSet, IMAGES
 
 from sqlalchemy import desc, asc
 import itertools
 import random
 import re
+import os
 
 app = Flask(__name__)
 app.config.from_envvar('WEDDING_SETTINGS')
 db = SQLAlchemy(app)
-photos = UploadSet('photos', IMAGES, default_dest='/tmp')
 
 class Greet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,15 +33,27 @@ class Greet(db.Model):
 def nl2br(value):
     return Markup('<br />'.join(escape(value).split('\n')))
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOADED_FILES_DEST'], filename)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     messages = []
     if request.method == 'POST':
         greet = Greet()
         if 'photo' in request.files:
-            filename = photos.save(request.files['photo'])
-            greet.is_image = True
-            greet.image = filename
+            f = request.files['photo']
+            if f and allowed_file(f.filename):
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOADED_FILES_DEST'], filename))
+                greet.is_image = True
+                greet.image = filename
+                db.session.add(greet)
         else:
             author = request.form['from']
             message = request.form['message']
@@ -50,7 +62,7 @@ def index():
             else:
                 greet.author = author
                 greet.message = message
-        db.session.add(greet)
+                db.session.add(greet)
         db.session.commit()
         messages.append(("Deine Nachricht ist angekommen! Danke.", 'success'))
     if request.user_agent.platform in ['android', 'iphone']:
